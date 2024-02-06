0efc39cdff09dcf7653d0c5f57e757bc7a26c096
AlexsanderDiego <alexdiegoat@gmail.com>
2024-02-06 01:02:10 -0300

    alternando api oficial

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
 
   async function login(dados) {
     try {
-      // const resposta = await axios.post("https://fs01backend.onrender.com/auth/login", dados);
-      const resposta = await axios.post("http://localhost:8080/auth/login", dados);
+      const resposta = await axios.post("https://fs01backend.onrender.com/auth/login", dados);
+      // const resposta = await axios.post("http://localhost:8080/auth/login", dados);
 
       // Manipule os dados da resposta conforme necessário
       console.log("Resposta do servidor:", resposta.data);
