5dcf2a3efb866c74ee93c5e3cc056ec2db61b2f1
AlexsanderDiego <alexdiegoat@gmail.com>
2024-02-16 00:42:27 -0300

    atualizar tela login

--- src/pages/TelaLogin.jsx
+++ src/pages/TelaLogin.jsx
@@ -6,7 +6,7 @@ import Cadastro from "./TelaCadastro";
 import ListarUsuarios from "./TelaListarUsuarios.jsx";
 
 //import do css
-import "./TelaLogin.css";
+import "./telalogin.css";
 import FormItem from "antd/es/form/FormItem/index.js";
 
 function AuthLogin() {
